1af64ab3d8348adeabd708dec4d3430042562336
Arnav Garg <94055667+auxi0ngg@users.noreply.github.com>
2026-08-16 02:17:48 +0530

    Time: 81 ms (89.66%), Space: 28.1 MB (83.2%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+
+        int sum = 0, maxCount = 0, n = nums1.size();
+
+        unordered_map<int,int> mpp;
+
+        for(int i = 0; i<n; i++){
+            for (int j = 0; j<n; j++){
+                mpp[nums1[i]+nums2[j]]++;
+            }
+        }
+
+        for(int i = 0; i<n; i++){
+            for (int j = 0; j<n; j++){
+                maxCount = maxCount + mpp[0 - (nums3[i]+nums4[j])];
+            }
+        }
+
+        return maxCount;
+        
+    }
+};
